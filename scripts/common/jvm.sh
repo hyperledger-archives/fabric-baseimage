1aaa86d341301d75c84124945bea7e70cdb1c3a9
Matthew Sykes <sykesmat@us.ibm.com>
2018-09-11 11:23:29 -0400

    [FABB-100] update JDK fix pack, update url munging
    Change-Id: Icf520128d74d1f0381906d73f3446042ffe16fba
    Signed-off-by: Matthew Sykes <sykesmat@us.ibm.com>

--- scripts/common/jvm.sh
+++ scripts/common/jvm.sh
@@ -16,14 +16,14 @@ ARCH=`uname -m | sed 's|i686|386|' | sed 's|x86_64|amd64|'`
 if [ $ARCH = s390x -o $ARCH = ppc64le ]; then
     # Java is required for node.bin below. InstallAnywhere requirement.
     # See https://github.com/ibmruntimes/ci.docker/blob/master/ibmjava/8-sdk/s390x/ubuntu/Dockerfile
-    JAVA_VERSION=1.8.0_sr5fp16
-    ESUM_s390x="5086a317a2fe0a42b1ea9a559dc87a5a349b25f1debcd109777acb7fa7349b20"
-    ESUM_ppc64le="c52b806779e0c7cc432cea225d4c26e8c3a2d39f6b8535263cc44ea127f7f786"
+    JAVA_VERSION=1.8.0_sr5fp21
+    ESUM_s390x="cb7ca73df6a488c3f7cf56ded07b6768161dfdb9af346992d823d0607a56bf72"
+    ESUM_ppc64le="45f5be093a7aeef49508bee482e7a930454745185e949fdd5aaa48635ba22bfd"
     eval ESUM=\$ESUM_$ARCH
     BASE_URL="https://public.dhe.ibm.com/ibmdl/export/pub/systems/cloud/runtimes/java/meta/"
     YML_FILE="sdk/linux/$ARCH/index.yml"
     wget -q -U UA_IBM_JAVA_Docker -O /tmp/index.yml $BASE_URL/$YML_FILE
-    JAVA_URL=$(cat /tmp/index.yml | sed -n '/'$JAVA_VERSION'/{n;p}' | sed -n 's/\s*uri:\s//p' | tr -d '\r')
+    JAVA_URL=$(cat /tmp/index.yml | sed -n '/^'${JAVA_VERSION}:'/{n;s/\s*uri:\s//p}' | tr -d '\r')
     wget -q -U UA_IBM_JAVA_Docker -O /tmp/ibm-java.bin $JAVA_URL
     echo "$ESUM  /tmp/ibm-java.bin" | sha256sum -c -
     echo "INSTALLER_UI=silent" > /tmp/response.properties
